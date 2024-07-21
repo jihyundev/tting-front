55848776deb44321b09fa60c1d5dc9d356852587
jihyundev <aawlgus12@gmail.com>
2024-07-21 16:28:15 +0900

    feat: add basic idea creation form

--- src/app/idea/create/page.tsx
+++ src/app/idea/create/page.tsx
@@ -1,4 +1,5 @@
 import {Typography} from "@/components/typography";
+import {IdeaCreateForm} from "@/components/idea-create-form";
 
 export default function CreatePage() {
     return (
@@ -6,13 +7,7 @@ export default function CreatePage() {
             <header className="w-full sticky top-0 flex justify-center items-center text-center">
                 <Typography variant="subtitle1">다양한 결과물을 만들어보세요</Typography>
             </header>
-            <div className="flex justify-center m-12 bg-white">
-                <div className="w-full h-full bg-grey-300">
-                    <div className="bg-yellow-200">입력</div>
-                    <div className="bg-red-200">요청사항</div>
-                </div>
-                <div className="w-full h-full bg-cyan-200">출력</div>
-            </div>
+            <IdeaCreateForm />
         </>
     )
 }
