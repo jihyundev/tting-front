35e350fd7771a844823e025332ae2966fadb7d4f
jihyundev <aawlgus12@gmail.com>
2024-07-23 00:10:50 +0900

    feat: add error, not found page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,9 @@
 'use client'
 
 import { useEffect } from 'react'
+import Image from "next/image";
+import {Typography} from "@/components/typography";
+import {Button} from "@/components/ui/button";
 
 export default function Error({ error, reset }: {
     error: Error & { digest?: string }
@@ -12,16 +15,18 @@ export default function Error({ error, reset }: {
     }, [error])
 
     return (
-        <div>
-            <h2>Something went wrong!</h2>
-            <button
-                onClick={
-                    // Attempt to recover by trying to re-render the segment
-                    () => reset()
-                }
-            >
-                Try again
-            </button>
+        <div className="w-full h-full flex justify-center align-center gap-8">
+            <Image
+                src="/brand/error-logo.svg"
+                alt="error-logo"
+                width={280}
+                height={200}
+            />
+            <div className="h-full flex flex-col justify-center align-center gap-6">
+                <Typography variant="header1">알 수 없는 에러가 발생했어요.</Typography>
+                <Typography variant="subtitle3">잠시 후에 다시 시도해주세요.</Typography>
+                <Button onClick={() => reset()}>다시 시도해보기</Button>
+            </div>
         </div>
     )
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,25 @@
 import Link from 'next/link'
+import Image from "next/image";
+import {Typography} from "@/components/typography";
+import {Button} from "@/components/ui/button";
 
 export default function NotFound() {
     return (
-        <div>
-            <h2>404 Not Found.</h2>
-            <p>Could not find requested resource</p>
-            <Link href="/">Return Home</Link>
+        <div className="w-full h-full flex justify-center align-center gap-8">
+            <Image
+                src="/brand/error-logo.svg"
+                alt="error-logo"
+                width={280}
+                height={200}
+            />
+            <div className="h-full flex flex-col justify-center align-center gap-6">
+                <Typography variant="header1">띠용? 이 페이지는 없는 것 같아요!</Typography>
+                <Typography variant="subtitle3">요청하신 페이지가 사라졌거나 잘못된 주소를 입력하셨습니다. <br/>
+                    홈으로 이동해 주세요 :)</Typography>
+                <Link href="/">
+                    <Button>홈으로 이동</Button>
+                </Link>
+            </div>
         </div>
     )
 }
