4c828a076f1e3be722b65d12efd0e8996f3c0e2b
jihyundev <aawlgus12@gmail.com>
2024-07-25 22:27:37 +0900

    fix: change tag color class generation style

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -3,6 +3,17 @@
 import {Typography} from "@/components/typography";
 import {TagColors} from "@/types/tag-colors";
 
+const colorClasses = {
+    color1: 'bg-color1',
+    color2: 'bg-color2',
+    color3: 'bg-color3',
+    color4: 'bg-color4',
+    color5: 'bg-color5',
+    color6: 'bg-color6',
+    color7: 'bg-color7',
+    color8: 'bg-color8',
+};
+
 export const Tag = ({ label, color, onClick }: {
     label: string
     color: TagColors
@@ -15,7 +26,7 @@ export const Tag = ({ label, color, onClick }: {
     }
     return (
         <button
-            className={`p-1.5 h-7 text-ellipsis overflow-hidden rounded-full bg-${color}`}
+            className={`p-1.5 h-7 text-ellipsis overflow-hidden rounded-full ${colorClasses[color]}`}
             onClick={onSelectTag}
         >
             <Typography variant="button2">{label}</Typography>
