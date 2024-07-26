f29b90f0515d93109b0facafebfe5105aca59985
Jihyeon Jeong <aawlgus12@gmail.com>
2024-07-26 16:39:06 +0900

    검색바 사용성 개선  (#15)
    * feat: add search bar
    
    * fix type error
    
    * feat: keep search text in the search bar after pressing enter
    
    * feat: add clear button to searchbar
    
    * fix: fix array type error

--- src/components/idea-list.tsx
+++ src/components/idea-list.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Image from "next/image";
+import {useState} from "react";
 import {useSearchIdeas} from "@/hooks/use-search-ideas";
 import {IdeaItemCard} from "@/components/idea-item-card";
 import {IntroCard} from "@/components/intro-card";
@@ -7,29 +8,13 @@ import {LoadingAnimation} from "@/components/loading-animation";
 import {SearchBar} from "@/components/search-bar";
 
 export const IdeaList = () => {
-    const {data, isLoading, isError, error, setSearchText} = useSearchIdeas({});
+    const [searchText, setSearchText] = useState("");
+    const {data, isLoading, isError, error, setSearchText: setSearchQueryText} = useSearchIdeas({});
 
-    if (isLoading) {
-        return (
-            <div className="h-full flex justify-center">
-                <div className="w-48 h-36">
-                    <LoadingAnimation />
-                </div>
-            </div>
-        )
-    }
-
-    if (isError) {
-        return <div className="h-full flex justify-center">
-            <Image
-                src="/brand/error-logo.svg"
-                alt="error-logo"
-                width={280}
-                height={200}
-            />
-            에러가 발생했어요. 에러: {error ? error.message : '알 수 없음'}
-        </div>
-    }
+    const handleSearch = (searchText: string) => {
+        setSearchText(searchText);
+        setSearchQueryText(searchText);
+    };
 
     if (!data?.data?.data?.length && !isLoading && !isError) {
         return <IntroCard />
@@ -37,15 +22,39 @@ export const IdeaList = () => {
 
     return (
         <>
-            <div>
-                <div className="sticky my-5 flex justify-center">
-                    <SearchBar onSearch={setSearchText} />
+            <div className="w-full">
+                <div className="sticky w-full my-5 flex justify-center">
+                    <SearchBar
+                        value={searchText}
+                        onSearch={handleSearch}
+                        onChange={setSearchText}
+                    />
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-                    {data?.data?.data?.length && data.data.data.map(idea => (
-                        <IdeaItemCard key={idea.id} idea={idea} />
-                    ))}
-                </div>
+                {data?.data?.data?.length && (
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                        {data.data.data.map(idea => (
+                            <IdeaItemCard key={idea.id} idea={idea} />
+                        ))}
+                    </div>
+                )}
+                {isLoading && (
+                    <div className="w-full h-full flex justify-center align-center">
+                        <div className="w-48 h-36">
+                            <LoadingAnimation />
+                        </div>
+                    </div>
+                )}
+                {isError && (
+                    <div className="w-full h-full flex justify-center align-center">
+                        <Image
+                            src="/brand/error-logo.svg"
+                            alt="error-logo"
+                            width={280}
+                            height={200}
+                        />
+                        에러가 발생했어요. 에러: {error ? error.message : '알 수 없음'}
+                    </div>
+                )}
             </div>
         </>
     );
--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,21 +1,27 @@
 'use client'
 
-import {useState} from "react";
 import {SearchIcon} from "@/components/icons";
+import {CircleX} from "lucide-react";
 
-export const SearchBar = ({ onSearch = () => {} }: {
-    onSearch?: (searchText: string) => void
+export const SearchBar = ({
+    value = "",
+    onSearch = () => {},
+    onChange = () => {}
+}: {
+    value?: string,
+    onSearch?: (searchText: string) => void,
+    onChange?: (searchText: string) => void
 }) => {
-    const [searchText, setSearchText] = useState("");
     const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchText(e.target.value);
+        onChange(e.target.value);
     };
 
     const onInputKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter") {
-            onSearch(searchText);
+            onSearch(value);
         }
     };
+  
     return (
         <div className="flex items-center justify-center py-3 px-3.5 bg-white border border-gray-200 rounded-full shadow-sm h-14 w-full max-w-3xl">
             <SearchIcon className="h-9 w-9" />
@@ -24,10 +30,16 @@ export const SearchBar = ({ onSearch = () => {} }: {
                 maxLength={50}
                 placeholder="아이디어 검색하기"
                 className="flex-1 ml-3 border-none outline-none text-2xl text-gray-600 placeholder-gray-400"
-                value={searchText}
+                value={value}
                 onChange={onInputChange}
                 onKeyDown={onInputKeydown}
             />
+            {value && (
+                <CircleX
+                    className="h-7 w-7 text-gray-500 cursor-pointer"
+                    onClick={() => onChange("")}
+                />
+            )}
         </div>
     )
 }
