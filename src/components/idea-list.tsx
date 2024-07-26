c0ab6210bfd588d879f2e577443587331323aa6f
jihyundev <aawlgus12@gmail.com>
2024-07-26 16:27:35 +0900

    fix: fix array type error

--- src/components/idea-list.tsx
+++ src/components/idea-list.tsx
@@ -30,7 +30,7 @@ export const IdeaList = () => {
                         onChange={setSearchText}
                     />
                 </div>
-                {data?.data?.data?.length > 0 && (
+                {data?.data?.data?.length && (
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
                         {data.data.data.map(idea => (
                             <IdeaItemCard key={idea.id} idea={idea} />
