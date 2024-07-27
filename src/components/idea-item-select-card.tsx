79c36fbb052518f45189e9db743868f7f7c55ec9
jihyundev <aawlgus12@gmail.com>
2024-07-27 18:03:50 +0900

    feat: add idea select mode

---
+++ src/components/idea-item-select-card.tsx
@@ -0,0 +1,38 @@
+import {IdeaItem} from "@/types/idea-fetch";
+import {Tag} from "@/components/tag";
+import {Card, CardHeader, CardFooter} from "@/components/ui/card";
+import {Separator} from "@/components/ui/separator";
+import {useState} from "react";
+
+export const IdeaItemSelectCard = ({idea, onSelect, onUnselect}: {
+    idea: IdeaItem,
+    onSelect: (idea: IdeaItem) => void,
+    onUnselect: (idea: IdeaItem) => void
+}) => {
+    const [isSelected, setIsSelected] = useState(false);
+    const onSelectCard = () => {
+        if (isSelected) {
+            setIsSelected(false);
+            onUnselect(idea);
+        } else {
+            setIsSelected(true);
+            onSelect(idea);
+        }
+    }
+    return (
+        <Card
+            className={`${isSelected && "ring"}`}
+            onClick={onSelectCard}
+        >
+            <CardHeader>
+                {idea.content}
+            </CardHeader>
+            <Separator/>
+            <CardFooter className="pt-6 flex align-center gap-1.5">
+                {idea.tags.map(tag => (
+                    <Tag key={tag.id} label={tag.name} color={tag.color} />
+                ))}
+            </CardFooter>
+        </Card>
+    )
+}
