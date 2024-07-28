35d527f31d20eed999a6ebb11c708cfa75009129
jihyundev <aawlgus12@gmail.com>
2024-07-28 13:53:35 +0900

    feat: add mock image to insight dashboard

--- src/app/insight/page.tsx
+++ src/app/insight/page.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+import {Typography} from "@/components/typography";
+
 export default function InsightPage() {
     return (
-        <div>인사이트 대시보드 페이지</div>
+        <div className="mt-44">
+            <Typography variant="subtitle1" className="text-center m-6 mb-10">
+                아이디어 통계와 분석을 준비 중이에요. 곧 만나요!
+            </Typography>
+            <Image
+                src={"/images/insight-dashboard-mock-image.png"}
+                alt={"insight-dashboard-mock-image"}
+                width={800}
+                height={400}
+                className="blur-sm"
+            />
+        </div>
     )
 }
