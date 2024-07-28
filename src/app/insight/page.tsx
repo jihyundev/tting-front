e7c2449de53c163d994a97a2d16a1cbb1eda27ae
jihyundev <aawlgus12@gmail.com>
2024-07-28 13:57:19 +0900

    feat: change top margin

--- src/app/insight/page.tsx
+++ src/app/insight/page.tsx
@@ -3,7 +3,7 @@ import {Typography} from "@/components/typography";
 
 export default function InsightPage() {
     return (
-        <div className="mt-44">
+        <div className="mt-40">
             <Typography variant="subtitle1" className="text-center m-6 mb-10">
                 아이디어 통계와 분석을 준비 중이에요. 곧 만나요!
             </Typography>
