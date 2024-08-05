dace3ab300bf600bbb9c652307a6a9d6c6a11fc3
jihyundev <aawlgus12@gmail.com>
2024-08-05 23:34:44 +0900

    bugfix: add token check on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,11 @@
 import { auth } from './auth';
-import { NextResponse } from 'next/server';
+import {NextRequest, NextResponse} from 'next/server';
 
-export async function middleware() {
+export async function middleware(request: NextRequest) {
     const session = await auth();
-    if (!session) {
+    const accessToken = request.cookies.get('accessToken');
+    const refreshToken = request.cookies.get('refreshToken');
+    if (!session || !accessToken || !refreshToken) {
         return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BASE_URL}/login`);
     }
 }
