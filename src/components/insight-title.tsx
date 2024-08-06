80cda64c36cb5659302516ac1993ce9e1f01ea9e
jihyundev <aawlgus12@gmail.com>
2024-08-06 23:51:38 +0900

    feat: add card wrapper to insight charts

--- src/components/insight-title.tsx
+++ src/components/insight-title.tsx
@@ -6,7 +6,7 @@ import {Typography} from "@/components/typography";
 export const InsightTitle = () => {
     const { name } = useUserProfile();
     return (
-        <Typography variant="subtitle1" className="text-center mb-10">
+        <Typography variant="subtitle1" className="text-center mb-7">
             {name || ''}님은 적극적으로 아이디어를 활용하고 있어요
         </Typography>
     )
