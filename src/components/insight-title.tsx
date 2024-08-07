b7942d818429bae317b19d732fd1074358cb4962
jihyundev <aawlgus12@gmail.com>
2024-08-07 23:57:29 +0900

    feat: check idea usage index for insight title text

--- src/components/insight-title.tsx
+++ src/components/insight-title.tsx
@@ -2,12 +2,16 @@
 
 import {useUserProfile} from "@/hooks/use-user-profile";
 import {Typography} from "@/components/typography";
+import {useInsightSummary} from "@/hooks/use-insight-summary";
 
 export const InsightTitle = () => {
     const { name } = useUserProfile();
+    const {data} = useInsightSummary();
+    const recentUsageIndex = data?.data.recentUsageIndex || 0;
+
     return (
         <Typography variant="subtitle1" className="text-center mb-7">
-            {name || ''}님은 적극적으로 아이디어를 활용하고 있어요
+            {recentUsageIndex > 100 ? `${name || ''}님은 아이디어를 적극적으로 활용하고 있어요` : '새로운 아이디어를 만들어 보세요'}
         </Typography>
     )
 }
