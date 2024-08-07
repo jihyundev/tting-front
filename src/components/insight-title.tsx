9a809af7e289a120ae902b8dacc61ec9a4f68c02
jihyundev <aawlgus12@gmail.com>
2024-08-08 00:33:29 +0900

    feat: change usage index condition for title text

--- src/components/insight-title.tsx
+++ src/components/insight-title.tsx
@@ -8,10 +8,22 @@ export const InsightTitle = () => {
     const { name } = useUserProfile();
     const {data} = useInsightSummary();
     const recentUsageIndex = data?.data.recentUsageIndex || 0;
+    const getText = () => {
+        if (!recentUsageIndex) {
+            return '';
+        }
+        if (recentUsageIndex < 50) {
+            return `${name || ''}님 새로운 아이디어를 추가해보세요.`;
+        }
+        if (recentUsageIndex < 150) {
+            return `${name || ''}님 다양한 아이디어를 조합하고 만들어보세요.`;
+        }
+        return `${name || ''}님은 아이디어를 적극적으로 활용하고 있어요`;
+    }
 
     return (
         <Typography variant="subtitle1" className="text-center mb-7">
-            {recentUsageIndex > 100 ? `${name || ''}님은 아이디어를 적극적으로 활용하고 있어요` : '새로운 아이디어를 만들어 보세요'}
+            {getText()}
         </Typography>
     )
 }
