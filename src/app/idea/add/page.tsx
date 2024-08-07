fea9b633984ab585b926784d20434e7d8a4a0895
jihyundev <aawlgus12@gmail.com>
2024-08-07 23:42:13 +0900

    fix: add suspense to idea add page

--- src/app/idea/add/page.tsx
+++ src/app/idea/add/page.tsx
@@ -1,9 +1,21 @@
 import {IdeaAddCard} from "@/components/idea-add-card";
+import {LoaderCircleIcon} from "lucide-react";
+import {Suspense} from "react";
+
+const CreateFormFallback = () => {
+    return (
+        <div className="flex justify-center items-center h-full">
+            <LoaderCircleIcon size={30} />
+        </div>
+    )
+}
 
 export default function IdeaAddPage() {
     return (
         <div className="w-full h-full flex justify-center items-center">
-            <IdeaAddCard />
+            <Suspense fallback={<CreateFormFallback />}>
+                <IdeaAddCard />
+            </Suspense>
         </div>
     )
 }
