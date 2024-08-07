c929d89e64bca2bb23d1c314dc19c47593215e9c
Jihyeon Jeong <aawlgus12@gmail.com>
2024-08-08 00:34:02 +0900

    feat: check idea usage index for insight title text (#37)
    * feat: check idea usage index for insight title text
    
    * feat: change usage index condition for title text

--- src/components/insight-title.tsx
+++ src/components/insight-title.tsx
@@ -2,12 +2,28 @@
 
 import {useUserProfile} from "@/hooks/use-user-profile";
 import {Typography} from "@/components/typography";
+import {useInsightSummary} from "@/hooks/use-insight-summary";
 
 export const InsightTitle = () => {
     const { name } = useUserProfile();
+    const {data} = useInsightSummary();
+    const recentUsageIndex = data?.data.recentUsageIndex || 0;
+    const getText = () => {
+        if (!recentUsageIndex) {
+            return '';
+        }
+        if (recentUsageIndex < 50) {
+            return `${name || ''}님 새로운 아이디어를 추가해보세요.`;
+        }
+        if (recentUsageIndex < 150) {
+            return `${name || ''}님 다양한 아이디어를 조합하고 만들어보세요.`;
+        }
+        return `${name || ''}님은 아이디어를 적극적으로 활용하고 있어요`;
+    }
+
     return (
         <Typography variant="subtitle1" className="text-center mb-7">
-            {name || ''}님은 적극적으로 아이디어를 활용하고 있어요
+            {getText()}
         </Typography>
     )
 }
