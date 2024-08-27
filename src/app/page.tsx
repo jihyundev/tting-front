8c32b2340a21ed6cef8532cfff15a10333836bdc
jihyundev <aawlgus12@gmail.com>
2024-08-27 22:51:40 +0900

    fix: add token check when first page load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import { getCookie } from 'cookies-next';
+import { cookies } from 'next/headers';
 import {auth} from "@/auth";
 import {IntroCard} from "@/components/intro-card";
 import {IdeaList} from "@/components/idea/idea-list";
 
 export default async function Page() {
     const session = await auth();
+    const hasToken = getCookie("accessToken", { cookies }) && getCookie("refreshToken", { cookies});
 
-    if (session?.user) {
+    if (session?.user && hasToken) {
         return (
             <div className="flex justify-center items-center mx-4 sm:mx-6 md:mx-8 lg:mx-12 pb-12">
                 <IdeaList />
